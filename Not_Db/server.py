4a31f27d9c0ae0da396b129390983f002c78a9bc
martyni <martynjamespratt@gmail.com>
2017-08-17 10:44:46 +0000

    sub sub domains

--- Not_Db/server.py
+++ Not_Db/server.py
@@ -167,7 +167,8 @@ api.add_resource(Book, '/<string:db>')
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*martyni.co.uk')
+    response.headers.add('Access-Control-Allow-Origin', '*.martyni.co.uk')
+    response.headers.add('Access-Control-Allow-Origin', '*.*.martyni.co.uk')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
