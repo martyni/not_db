0b5bbf296f6894efa5d5857bb34a52804cdcd835
martyni <martynjamespratt@gmail.com>
2017-08-29 15:43:05 +0000

    added test

--- jenkins/test.py
+++ jenkins/test.py
@@ -1,12 +1,20 @@
 import requests
 import sys
-
+import json
 print "url: " + sys.argv[1]
 base_url = sys.argv[1]
 
 def test_path(path):
     if requests.get(base_url + path):
         print "success: {}".format(base_url + path) 
+        return 1
+    else:
+        print "fail: {}".format(base_url + path)
+        return 0
+
+def put(path, payload)
+    if requests.put(base_url + path, data=json.dumps(payload)):
+        print "success: {}".format(base_url + path)
         return 1
     else:
         print "fail: {}".format(base_url + path)
@@ -28,11 +36,15 @@ def check_this_errors(path):
     else:
         print "no errors: {}".format(base_url + path)
         return 1
-'''
-for path in [""]:
-    if not test_path(path):
+
+for path in ["/aklsdjhfalkjsdfhlakjhdsfl/list/dave"]:
+    if not put(path, "hi"):
         sys.exit(1)
 
+for path in ["/aklsdjhfalkjsdfhlakjhdsfl/list/dave"]:
+    if not get(path):
+        sys.exit(1)
+'''
 for path in [""]:
     if not serializable(path):
         sys.exit(1)
