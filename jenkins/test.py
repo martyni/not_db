c944106c0f1ade438b89e3cac9ac742a9992fffb
martyni <martynjamespratt@gmail.com>
2017-08-29 16:03:03 +0000

    updated tests

--- jenkins/test.py
+++ jenkins/test.py
@@ -4,21 +4,37 @@ import json
 print "url: " + sys.argv[1]
 base_url = sys.argv[1]
 
-def test_path(path):
-    if requests.get(base_url + path):
+def get(path, payload=None):
+    req = requests.get(base_url + path)
+    if req and not payload:
         print "success: {}".format(base_url + path) 
         return 1
+    elif req and payload:
+        if payload not in req.text:
+            print "fail: {}".format(base_url + path)
+            print "{} not in {}".format(payload, req.text)
+            return 0
+        else:    
+            print "success: {}".format(base_url + path) 
+            return 1
     else:
         print "fail: {}".format(base_url + path)
         return 0
 
-def put(path, payload)
-    if requests.put(base_url + path, data=json.dumps(payload)):
+def put(path, payload):
+    if requests.put(base_url + path, data="data=" + json.dumps(payload)):
         print "success: {}".format(base_url + path)
         return 1
     else:
         print "fail: {}".format(base_url + path)
         return 0
+
+def delete(path):
+    if requests.delete(base_url + path):
+        print "success: {}".format(base_url + path)
+        return 1
+    else:
+        print "fail: {}".format(base_url + path)
 
 def serializable(path):
     try:
@@ -36,14 +52,22 @@ def check_this_errors(path):
     else:
         print "no errors: {}".format(base_url + path)
         return 1
+paths = ["aklsdjhfalkjsdfhlakjhdsfl"]
+lists = ["dave"]
+complete_paths = ["/{}/list/{}".format(path, list_) for path in paths for list_ in lists ]
 
-for path in ["/aklsdjhfalkjsdfhlakjhdsfl/list/dave"]:
+for path in complete_paths:
     if not put(path, "hi"):
         sys.exit(1)
 
-for path in ["/aklsdjhfalkjsdfhlakjhdsfl/list/dave"]:
-    if not get(path):
+for path in complete_paths:
+    if not get(path, "hi"):
         sys.exit(1)
+
+for path in paths:
+    if not delete("/" + path):
+        sys.exit(1)
+
 '''
 for path in [""]:
     if not serializable(path):
